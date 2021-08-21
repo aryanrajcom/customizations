ffe0d52e724a0ef9e336da6562d7783311c1b8f2
Aryan Raj <contact@aryanraj.com>
2021-08-21 15:34:54 +0530

    Update global.js

--- assets/js/global.js
+++ assets/js/global.js
@@ -1,12 +1,14 @@
+// Only jQuery scripts should be placed under the Examples. You can directly put JS code that doesn't require jQuery.
+
 /**
  * Example 1.
  */
 ( function ( $ ) {
 	
     'use strict';
-    
+
 	/* Put Some function here.
-    --------------------------------------------- */
+	--------------------------------------------- */
     
 } )( jQuery );
 
